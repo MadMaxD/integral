68c485a30952dc88ec1e4b886dcf607bb8326730
MadMaxD <sokrushitel07@gmail.com>
2016-02-16 00:38:16 +0500

    Пример преподавателя

--- Program.cs
+++ Program.cs
@@ -10,6 +10,40 @@ namespace IntDeleg
     {
         static void Main(string[] args)
         {
+            A a= new A('s');
+            Console.WriteLine(a.P(5));
+            Console.ReadKey();
         }
+
     }
+
+    internal class A
+    {
+        public delegate int Power(int x);
+
+        public Power P;
+
+        private int square(int x)
+        {
+            return x*x;
+        }
+
+        private int cube(int x)
+        {
+            return x*x*x;
+        }
+
+        public A(char f) 
+        {
+            if (f == 's')
+            {
+                P = new Power(square);
+            }
+            else if (f == 'c')
+            {
+                P = new Power(cube);
+            }
+       }
+    }
+
 }
